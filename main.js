a6492d09156d3fb63cc0beb47703725bdd9ecc37
unknown <rmuhammadali316@gmail.com>
2025-05-22 15:04:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const dropdown= document.querySelector(".dropdown-items1")
+const dropdown1= document.querySelector(".dropdown-items2")
+const dropdown2= document.querySelector(".dropdown-items3")
+
+dropdown.addEventListener("click", ()=>{
+    dropdown1.classList.add()
+})
